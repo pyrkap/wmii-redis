360eaceaa5eb41e688957918ae75fdd8dc577f94
Patryk Pyrka <patryk.pyrka@allegro.com>
2025-10-25 13:05:47 +0200

    Initial commit

---
+++ src/test/java/pyrkap/wmii_workshops/redis/RedisApplicationTests.java
@@ -0,0 +1,13 @@
+package pyrkap.wmii_workshops.redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
